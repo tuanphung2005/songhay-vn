fb3c88afbfc6329bb2e5634a831c42c263931094
PHUNG ANH TUAN <tuan29102005@gmail.com>
2026-05-01 20:58:19 +0700

    fix: added _next cache to noindex

--- app/robots.ts
+++ app/robots.ts
@@ -9,7 +9,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/admin", "/login", "/api/"],
+        disallow: ["/admin", "/login", "/api/", "/_next/"],
       },
     ],
     host: siteUrl,
